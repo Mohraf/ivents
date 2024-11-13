904d646d9877aee403d8831331fc25637bc8c636
mohraf <amosngok@gmail.com>
2024-11-13 19:44:46 +0300

    (fix/navbar): set services bg and color

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -38,12 +38,12 @@ const Navbar = () => {
               <MdArrowDropDown className="ml-1" />
             </button>
             {isDropdownOpen && (
-              <div className="absolute left-0 mt-2 w-40 bg-black bg-opacity-70 border border-gray-500 rounded shadow-lg">
-                <Link href="/audioAndVisual" className="block px-4 py-2 hover:bg-gray-500 hover:bg-opacity-50">Audio & Visual</Link>
-                <Link href="/conferencesAndExhibitions" className="block px-4 py-2 hover:bg-gray-500 hover:bg-opacity-50">Conferences & Exhibitions</Link>
-                <Link href="/stageAndLighting" className="block px-4 py-2 hover:bg-gray-500 hover:bg-opacity-50">Stage & Lighting</Link>
-                <Link href="/printingAndBranding" className="block px-4 py-2 hover:bg-gray-500 hover:bg-opacity-50">Printing & Branding</Link>
-                <Link href="/productLaunches" className="block px-4 py-2 hover:bg-gray-500 hover:bg-opacity-50">Products & Launches</Link>
+              <div className="absolute left-0 mt-2 w-40 bg-white text-black bg-opacity-90 border border-gray-500 rounded shadow-lg">
+                <Link href="/audioAndVisual" className="block px-4 py-2 hover:bg-gray-400 hover:bg-opacity-50">Audio & Visual</Link>
+                <Link href="/conferencesAndExhibitions" className="block px-4 py-2 hover:bg-gray-400 hover:bg-opacity-50">Conferences & Exhibitions</Link>
+                <Link href="/stageAndLighting" className="block px-4 py-2 hover:bg-gray-400 hover:bg-opacity-50">Stage & Lighting</Link>
+                <Link href="/printingAndBranding" className="block px-4 py-2 hover:bg-gray-400 hover:bg-opacity-50">Printing & Branding</Link>
+                <Link href="/productLaunches" className="block px-4 py-2 hover:bg-gray-400 hover:bg-opacity-50">Products & Launches</Link>
               </div>
             )}
           </div>
