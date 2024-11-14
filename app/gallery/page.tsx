86ac35e6749af65e529a443ed68604a45c7b271e
mohraf <amosngok@gmail.com>
2024-11-14 13:17:20 +0300

    added gallery text

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -138,6 +138,7 @@ const Gallery = () => {
         <div className="p-6 container mx-auto">
           <div className="py-2">
             <h1 className="text-center text-4xl text-white">Our Gallery</h1>
+            <p className="text-xl text-center text-white mt-5">Take a Look at Our Latest Events</p>
           </div>
           <div className="md:grid md:gap-6 md:grid-cols-2 lg:grid-cols-3 mb-12">
             {data.map((x) => {
