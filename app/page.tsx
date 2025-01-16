2e9292bac0437c2d3d80d7ac56cfff5130c0c785
mohraf <amosngok@gmail.com>
2025-01-16 16:56:23 +0300

    (chore/performance): add suspense with skeleton  to root page to minimize TTFB

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import TestimonialSlider from "@/components/Testimonials/TestimonialSlider";
 import ServicesSlider from "@/components/Services/ServicesSlider";
 import Footer from "@/components/Footer/Footer";
@@ -6,17 +7,46 @@ import Intro from "@/components/Intro/Intro";
 import Hero from "@/components/Hero/Hero";
 import Experiences from "@/components/Experiences/Experiences";
 import Events from "@/components/Events/Events";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Intro />
-      <Experiences />
-      <Brands />
-      <ServicesSlider />
-      <Events />
-      <TestimonialSlider />
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <Hero />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <Intro />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <Experiences />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <Brands />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <ServicesSlider />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <Events />
+      </Suspense>
+      <Suspense
+        fallback={<Skeleton className="w-full h-48 bg-gray-700 rounded-lg" />}
+      >
+        <TestimonialSlider />
+      </Suspense>
     </>
   );
 }
